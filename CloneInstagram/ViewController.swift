d1a8bccf157478542ed6e4dee7e2ec6f404e9f79
hemg2 <hemg2@naver.com>
2022-11-17 14:25:22 +0900

    Initial Commit

---
+++ CloneInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloneInstagram
+//
+//  Created by 1 on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
